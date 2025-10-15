81e4d6dabb11a33f4192d968709e8eb661951111
zehranrsoydan <znsoydann@gmail.com>
2025-10-15 19:28:41 +0300

    Update products.js

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -1,11 +1,11 @@
 import api from './client';
 
 export const fetchProducts = async (params = {}) => {
-  const res = await api.get('/products', { params });
+  const res = await api.get('/api/products', { params }); // DÜZELTİLDİ
   return res.data;
 };
 
 export const fetchProductDetail = async (slug) => {
-  const res = await api.get(`/products/${slug}`);
+  const res = await api.get(`/api/products/${slug}`); // DÜZELTİLDİ
   return res.data;
 };
