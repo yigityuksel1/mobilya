83b8011a8444e90e6f6498090641cde2b33490d5
zehranrsoydan <znsoydann@gmail.com>
2025-10-15 19:21:09 +0300

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,8 +4,6 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'https://dumanmobilya.ddev.site',
