1848adb10d05e62b066adb4767abfc59f93b1771
zehranrsoydan <znsoydann@gmail.com>
2025-10-16 15:00:54 +0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-console.log('MAIN TSX LOADED');
-
-function Marker() {
-  return (
-    <div style={{ padding: 24, background: '#222', color: '#0ff', fontSize: 28 }}>
-      MAIN.TSX WORKS
-    </div>
-  );
-}
+import Login from './pages/Login';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Marker />
+    <Login />
   </React.StrictMode>
 );
