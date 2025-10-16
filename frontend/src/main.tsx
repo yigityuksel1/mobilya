471b1eb8445633ff877aba72c4d417e51048e1bb
zehranrsoydan <znsoydann@gmail.com>
2025-10-16 14:46:34 +0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
 import Login from './pages/Login';
-import Me from './pages/Me';
-
-const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/login', element: <Login /> },
-  { path: '/me', element: <Me /> },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Login />
   </React.StrictMode>
 );
